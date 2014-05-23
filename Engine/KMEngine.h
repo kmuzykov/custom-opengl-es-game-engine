ab2b1669be5b97663a6f2f75e2092d260f41f83b
Kirill Muzykov <kmuzykov@gmail.com>
2014-05-23 20:23:55 +0400

    Added one header to include if you want to use engine.

---
+++ Engine/KMEngine.h
@@ -0,0 +1,29 @@
+//
+//  KMEngine.h
+//  Arkanoid
+//
+//  Created by Kirill Muzykov on 23/05/14.
+//  Copyright (c) 2014 Kirill Muzykov. All rights reserved.
+//
+
+#ifndef KMEngine_KMEngine_h
+#define KMEngine_KMEngine_h
+
+#include "KMDirector.h"
+#include "KMScene.h"
+#include "KMGameObject.h"
+
+
+#include "KMTexture.h"
+#include "KMShader.h"
+#include "KMVertex.h"
+
+#include "KMRenderer.h"
+#include "KMRendererMesh.h"
+
+#include "KMMaterial.h"
+#include "KMMaterialColorUniform.h"
+#include "KMMaterialTextureUnlit.h"
+#include "KMMaterialTextureDiffuse.h"
+
+#endif
